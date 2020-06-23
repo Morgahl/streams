411cc200764991845e88cba4f77b040c94334618
Marc Hayes <marc.hayes.tech@gmail.com>
2020-06-23 00:15:13 -0600

    Tee File should just read until EOF.

--- tee_file_closer.go
+++ tee_file_closer.go
@@ -7,50 +7,37 @@ import (
 )
 
 var (
-	// compile time checking of io.ReadCloser compliance
-	_ io.ReadCloser = new(TeeFileCloser)
+	// compile time checking of io.Reader compliance
+	_ io.Reader = new(TeeFileReader)
 )
 
-// TeeFileCloser wraps an io.TeeReader and automatically handles the close of the
+// TeeFileReader wraps an io.TeeReader and automatically handles the close of the
 // underlying reader and writer if they happen to be an io.Closer
-type TeeFileCloser struct {
+type TeeFileReader struct {
 	r      io.Reader
 	fClose closeFunc
-	close  closeFunc
 }
 
-// NewTeeFileCloser returns a new TeeFileCloser
-func NewTeeFileCloser(r io.Reader, path string) (io.ReadCloser, error) {
+// NewTeeFileReader returns a new TeeFileReader
+func NewTeeFileReader(r io.Reader, path string) (io.Reader, error) {
 	f, err := file.NewWriterCreate(path)
 	if err != nil {
 		return nil, err
 	}
 
-	return &TeeFileCloser{
+	return &TeeFileReader{
 		r:      io.TeeReader(r, f),
 		fClose: f.Close,
-		close: func() error {
-			if err := closeIfCloser(r); err != nil {
-				return err
-			}
-
-			return f.Close()
-		},
 	}, nil
 }
 
 // Read implements the io.Reader interface, it will close the underlying file
 // automatically if io.EOF is recieved
-func (tfc *TeeFileCloser) Read(p []byte) (n int, err error) {
+func (tfc *TeeFileReader) Read(p []byte) (n int, err error) {
 	if n, err = tfc.r.Read(p); err == io.EOF {
 		if cErr := tfc.fClose(); cErr != nil {
 			return n, cErr
 		}
 	}
 	return n, err
-}
-
-// Close implements io.Closer interface
-func (tfc *TeeFileCloser) Close() error {
-	return tfc.close()
 }
